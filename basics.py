6933128a853a7af4983c709d5e7c6c0fd289480c
Nikhil <np3112041@gmail.com>
2026-05-19 17:44:10 +0530

    first commit

---
+++ basics.py
@@ -0,0 +1,195 @@
+# print("hello world")
+#  bhagatsujal
+#  the best
+# no oimptions 
+# i achive my goal
+# age = 4
+# print(age)
+# name ="sujal"
+# profession = "student"
+# studying = " 3rd year"
+# print("hello  I am",name  , profession,"studying in ",studying)
+# x=5
+# print(type(x))
+# x = 5.5
+# print(type(x))
+# x = True
+# print(type(x))
+# x = tuple
+# print(type(x))
+# x = 1j
+# print(type(x))
+# x = "apple" "banana" "kiwi"
+# print(type(x))
+# x=3
+# print(x>6 and x>6)
+# print(x<6 or x<6)
+# print(x<3 and x<6)
+# print(not(x<3 or x<6))
+# x = ["maruti", "bmw"]
+# y = [ " maruti", "bmw"]
+# print(type(y))
+# dict = {
+#     "Name": "sujal",
+#     "subject": "iot",
+#     "learning": "python",
+#     "hobbies": ["reading", "playing","coding"],
+#     "grade": "A+"
+# }
+# # print(dict["Name"])
+# # print(dict["subject"])     
+# # print(dict["learning"])
+# # print(dict["hobbies"])
+# # print(dict["grade"])
+
+# # A = {1 , 2 , 3 , 4 }
+# # B = {3 , 4 , 5 , 6 , 7}
+# # print("Union:", A | B)
+# # print("Intersection:", A & B)
+# # a= int(input(" enter first number"))
+# # b=int(input("enter second number"))
+# # c = (a**2 + b**2)**0.5
+# # print(c)
+# # 
+# # print("+"+"-"*10+"+")
+# # print(("|"+" "*10+"|\n")*5,end="")
+# # print("+"+"-"*10+"+")
+# number1 = int(input("enter the number:"))
+# number1 = int(input("enter the number:"))
+# number2 = int(input("enter the number:"))
+# number3 = int(input("enter the number:"))
+# largest_number =number1
+# if number2 > largest_number:
+#     largest_number = number2
+# if number3 > largest_number:
+#     largest_number = number3
+# print("the largest is:", largest_number)    
+# largest_number = max(number1,number2,number3)
+# lowest_number = min(number1,number2,number3)
+# print("thelargest number is:", largest_number)
+# print("the lowest number is:", lowest_number)
+# p
+# largest_number = -999999999
+# number = int(input("enter the number"))
+# if number == -1:
+#     print("largest_number")
+#     exit()
+# if number > largest_number:1
+#     largest_number = number    
+# while True:
+#     print("im stuck in a loop")
+# largest_number =-999999999
+# number = int(input("Enter the the number or type -1 to stop: "))
+# while number != -1:
+#     if number > largest_number:
+#         largest_number = number
+#     number = int(input("Enter a number or type -1 to stop: ")) 
+#     print("the largest number is:",largest_number)
+# i =1
+# while i<= 50:
+#     print(i)
+#     i+= 1
+# i = 1
+# while i<= 10/2:
+#     print("even")
+# else:
+#     print("odd")
+# 
+# for counter in range(100):
+#     print("counter:", counter) 
+# power = 1
+# for expo in range(16):
+#     print("2 to the power of ",expo,"is",power)
+#     power *= 2
+# print("The break instruction:")
+# for counter in range(1,6):
+#      if counter== 3:
+#            #break
+#            continue
+#      print("inside the loop.", counter)
+# print("out side the loop.")    
+
+# var = 5
+# print(var >0)
+# print(not(var <= 0))
+
+# print(var !=0)
+# print(not(var == 0))
+# numbers= []
+# print(numbers)
+numbers = [10 , 5,7,2,1]
+# # print(numbers)
+# # print(type(numbers))
+
+# print("first element content:", numbers[0])
+# print("second element content:",numbers[1])
+# print("third element content:", numbers[2])
+# print("fourth element content:",numbers[4])
+
+# numbers[0] =111
+# print("numbers[0]:",numbers[0])
+# print(numbers)
+
+# numbers[1] = numbers[4]
+# print(numbers)
+# print(numbers[-1])
+# print(numbers[-2])
+# print(numbers[-3])
+# print(numbers[-4])
+
+# numbers = [1 , 2 ,3 , 4 ,5]
+# print(numbers)
+# print(len(numbers))
+# del numbers[4]
+# print(numbers)
+# numbers[2] = 5
+# print(numbers)
+# print(numbers[-1])
+
+# list = [ 5 , 4 , 3, 2 , 1]
+# print(list)
+# list.append(6)
+# print(list)
+# list.insert(0,10)
+# print(list)
+# my_list = [1,2,3,4,5,6,7,8,9,10]
+# for itreator in range (len(my_list)):
+#     print(my_list[itreator])
+# list = [ 1 ,2,3,4,5,6,7,8,9,10]
+# list.insert(10,0)
+# print(list)
+# list = []
+# for itreator in range(1,11):
+#     list.append(itreator)
+# print(list)
+# list = []
+# for itreator in range(10):
+#     list.append(itreator+1)
+# print(list)
+
+# list = [10 ,20,30,40,50,60,70,80,90,100]
+# for itreator in range(len(list)):
+#     list[itreator] += 1
+# print(list)  
+# list = [10 ,20,30,40,50,60,70,80,90,100]
+# sum = 0
+# for itreator in range(len(list)):
+#     sum += list[itreator]
+# print(sum)
+# my_list =[10,1,8,3,5]
+# total = 0
+# for element in my_list:
+#     total += element
+# print(total)
+# variable1 = 1
+# variable2 = 2
+# print("Variable1",variable1)
+# print("Variable2",variable2)
+# variable1,variable2 = variable2,variable1
+# print("Variable1",variable1)
+# print("Variable2",variable2)
+# list = [10 ,20,30,40,50,60,70,80,90,100]
+# list[4],list[1] = list[1],list[4]
+# print(list)
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
---
+++ practice.py
@@ -0,0 +1,43 @@
+# name = "sujal"
+# age = "20"
+# course = "btech"
+# print(name,age,course)
+# name1 = "Himani"
+# age1 = "20"
+# course1 = "btech"
+# name2 = "nikhil"
+# age2 = "27"
+# course2 = "bcom"
+# name3 = "hemant ji"
+# age3 = "27"
+# course3 = "mechinacal"
+# print(name1 , age1 ,course1)
+# print(name2,age2 ,course2)
+# print(name3,age3,course3)
+# 
+
+# print(a +b)
+# a = 5.5
+# b = 8.8
+# print(a*b)
+# a = 15 
+# b=16
+# print(a+b)
+# a = 15
+# b = 16
+# print(a-b)
+# print("*"*6)
+# print(("*"+" "*4+"*\n")*3,end="")print("*"*6)
+# print("*"*6)
+# print(("*"*5+"*\n ")*6,end="")
+# print("#"*6)
+# print(("#"*5+"#\n")*6,end="")
+# for i in range(1, 6):
+#     for j in range(1 , i + 1):
+#         print(j , end=" ")
+#         print()
+# for temp in range(1,7):
+#     print(str(temp)*temp)
+# my_list = [1,2,3,4,5,6,7,8,9,10]
+# for itreator in range (len(my_list)):
+#     print(my_list[itreator])
