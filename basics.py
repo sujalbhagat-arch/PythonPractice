32c28c7cb982c716744f83283420f120ff882a3d
Nikhil <np3112041@gmail.com>
2026-05-21 18:07:15 +0530

    final list

--- basics.py
+++ basics.py
@@ -242,7 +242,83 @@ numbers = [10 , 5,7,2,1]
 # my_list = [10,8,6,4,2]
 # del my_list[:]
 # print(my_list)
-my_list = [0, 3 , 12, 8, 2]
-print(5 in my_list)
-print(5 not in my_list)
-print(12 in my_list)
+# my_list = [0, 3 , 12, 8, 2]
+# print(5 in my_list)
+# print(5 not in my_list)
+# print(12 in my_list)
+# row = []
+# for i in range(8):
+#     row.append("WHITE_PWAN")
+# print(row)
+# row = ["WHITE_PAWN" for i in range(8)]
+# print(row)
+# squares = [index ** 2 for index in range(1,11)]
+# print(squares)
+# twos = [2 ** index for index in  range(8)]
+# print(twos)
+# squares = [index ** 2 for index in range(1,11)]
+# odd = [element for element in squares if element%2 != 0]
+# print(odd)
+# board = []
+# for i in range(8):
+#     row = ["EMPTY" for i in range (8)]
+#     board.append(row)
+# for index in board:
+#     print(index)
+# board[0][0] = "ROOK"
+# board[0][7] = "ROOK"
+# board[7][0] = "ROOK"
+# board[7][7] = "ROOK"
+# print("--------")
+# for element in board:
+#     print(element)
+# board[0][1] = "KINGHT"
+# board[0][6] = "KINGHT"
+# board[7][1] = "KINGHT"
+# board[7][6] = "KINGHT"
+# print("--------")
+# for element in board:
+#     print(element)
+# temp = [[0.0 for h in range(24)]for d in range(31)]
+# for element in temp:
+#     print(element)
+# temps = [[0.0 for h in range(24)]for d in range(31)]
+# temp1 = 30
+# temp1 = 19
+# temp2 = 32
+# count = 0
+# for days in temps:
+#     if count == 0:
+#         days[11] = temp1
+#         count = 1
+#     else:
+#         days[11] = temp2
+#         count = 0
+# for element in temps:
+#     print(element)
+# total = 0.0
+# for day in  temps:
+#     total += day[11]
+# average = total / 31
+# print("Average temperature at noon:",average)
+
+# highest = -100.0
+# for day in temps:
+#     for temp in day:
+#         if temp > highest:
+#             highest = temp
+# print("the highest temprature was:", highest)
+# Hot_days = 0
+# for day in temps:
+#     if day[11]>20.0:
+#         Hot_days += 1
+# print(Hot_days,"days were hot days in month.")
+rooms = [[[False for r in range(20)]for f in range(15)]for t in  range(3)]
+print(rooms)
+rooms[1] [9] [13] = True
+rooms[0] [4] [1] = True
+vacancy = 0
+for room_number in range(20):
+    if not rooms [1][9][room_number]:
+        vacancy += 1
+print("Vacancy in 3rd 15th floor of 3rd building",vacancy)        
